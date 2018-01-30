90fdb3f5dbe97065b8e0ac1bdd6703e0b217df0f
Justin Bissonnette <bissonnette22@hotmail.com>
2018-01-30 10:14:56 -0700

    initial commit

---
+++ mod4/multibranch.c
@@ -0,0 +1,42 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  multibranch.c
+ *
+ *    Description:  nested branches
+ *
+ *        Version:  1.0
+ *        Created:  01/30/2018 10:10:22 AM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Justin Bissonnette (), Justinbissonnette@mail.weber.edu
+ *   Organization:  wsu
+ *
+ * =====================================================================================
+ */
+#include <stdio.h>
+
+// Constants
+
+// Function Prototypes
+
+// Main Function
+int main()
+{
+    int income = 30000;
+    if (income)
+    {
+        printf("income is %d\n", income);
+
+    }
+    else
+    {
+        printf("no income");
+    }
+
+    return 0;
+}
+// Function Definitions
+
+
